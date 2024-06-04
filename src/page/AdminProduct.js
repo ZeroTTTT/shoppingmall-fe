6d140c8e0156026a8246c5d52df9a1d25a90d8b3
Youngho Park <dudgh4651@gmail.com>
2024-06-04 17:52:16 +0900

    15. 상품 보여주기 16. 상품 검색하기

--- src/page/AdminProduct.js
+++ src/page/AdminProduct.js
@@ -35,12 +35,18 @@ const AdminProduct = () => {
 
   //상품리스트 가져오기 (url쿼리 맞춰서)
   useEffect(()=>{
-    dispatch(productActions.getProductList())
-  },[])
+    dispatch(productActions.getProductList({ ...searchQuery }))
+  },[query])
 
 
   useEffect(() => {
     //검색어나 페이지가 바뀌면 url바꿔주기 (검색어또는 페이지가 바뀜 => url 바꿔줌=> url쿼리 읽어옴=> 이 쿼리값 맞춰서  상품리스트 가져오기)
+    if (searchQuery.name === ''){
+      delete searchQuery.name;
+    }
+    const params = new URLSearchParams(searchQuery);
+    const query = params.toString();
+    navigate('?' + query)
   }, [searchQuery]);
 
   const deleteItem = (id) => {
@@ -62,6 +68,10 @@ const AdminProduct = () => {
   const handlePageClick = ({ selected }) => {
     //  쿼리에 페이지값 바꿔주기
   };
+  //searchbox에서 검색어를 읽어온다 => 엔터를 치면 => searchQuery객체가 업데이트가 됨 {name: 스트레이트 팬츠}
+  //=>searchQuery객체 안에 아이템 기준으로 url을 새로 생성해서 호출 $name=스트레이트+팬츠 
+  //=> url쿼리 읽어오기
+  //=> url쿼리 기준으로 BE에 검색조건과 함께 호출한다.
 
   return (
     <div className="locate-center">
@@ -87,8 +97,8 @@ const AdminProduct = () => {
         <ReactPaginate
           nextLabel="next >"
           onPageChange={handlePageClick}
-          pageRangeDisplayed={5}
-          pageCount={100}
+          pageRangeDisplayed={5} //몇개의 페이지를 보여줄지
+          pageCount={100} //전체페이지
           forcePage={2} // 1페이지면 2임 여긴 한개씩 +1 해야함
           previousLabel="< previous"
           renderOnZeroPageCount={null}
